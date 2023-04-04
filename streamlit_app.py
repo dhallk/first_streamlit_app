0235bbecc6ff37edf9902df5292a4035e4699f1d
dhallk <120494014+dhallk@users.noreply.github.com>
2023-04-04 14:16:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,20 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
+
+# Get and display User/account and region details
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+# Get data from a table
+my_cur.execute("SELECT * from fruit_load_list")
+
+# Can use fetchone() / fetchall() depending on n of records
+my_data_rows = my_cur.fetchall()
+
+streamlit.text("The fruit load list contains:")
+# List all the data as dataframe
+streamlit.dataframe(my_data_rows)
+
